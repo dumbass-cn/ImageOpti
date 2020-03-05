930b3dea66a5b05597a2701db3082a15ac258fec
dumbass.cn <dumbass.cn@outlook.com>
2020-03-05 16:36:44 +0800

    initial commit

---
+++ Aunt/Aunt.h
@@ -0,0 +1,19 @@
+//
+//  Aunt.h
+//  Aunt
+//
+//  Created by dumbass.cn on 3/5/20.
+//  Copyright © 2020 dumbass.cn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Aunt.
+FOUNDATION_EXPORT double AuntVersionNumber;
+
+//! Project version string for Aunt.
+FOUNDATION_EXPORT const unsigned char AuntVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Aunt/PublicHeader.h>
+
+
---
+++ ImageOpti/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageOpti
+//
+//  Created by dumbass.cn on 3/5/20.
+//  Copyright © 2020 dumbass.cn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
